640f6d059a0ec4a39bf3e55afdb5bcfac5706e14
sharui <sr_sharui@163.com>
2017-03-24 14:26:29 +0800

    创建项目

---
+++ GuidePages/GuidePages/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GuidePages
+//
+//  Created by sharui on 2017/3/24.
+//  Copyright © 2017年 sharui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GuidePages/GuidePages/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GuidePages
+//
+//  Created by sharui on 2017/3/24.
+//  Copyright © 2017年 sharui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
